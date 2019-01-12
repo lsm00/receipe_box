0fe0a45496e00b9953be0f1cebd0bd29bf7281a9
lsm00 <mctwist98@gmx.de>
2019-01-12 18:06:08 +0100

    Navbar SignIn / SignOut fixed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
 
